730d727e4323be85aa3316848c97aa7949034b33
psy0370 <masanao.ichimura@gmail.com>
2021-03-26 21:55:10 +0900

    プロジェクト名変更・UI実装

---
+++ ImageConvertor/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using ControlzEx.Theming;
+using MaterialDesignThemes.Wpf;
+using System.Windows;
+
+namespace ImageFormatChanger
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // MahApps.MetroのテーマをWindowsの設定に追従させる
+            ThemeManager.Current.ThemeSyncMode = ThemeSyncMode.SyncWithAppMode;
+            ThemeManager.Current.ThemeChanged += Current_ThemeChanged;
+            ThemeManager.Current.SyncTheme();
+        }
+
+        /// <summary>
+        /// テーマが変更されたときの処理を定義します。
+        /// </summary>
+        private void Current_ThemeChanged(object sender, ControlzEx.Theming.ThemeChangedEventArgs e)
+        {
+            // Material DesignのテーマをMahApps.Metroのテーマに追従させる
+            var helper = new PaletteHelper();
+            var theme = helper.GetTheme();
+            var bTheme = e.NewTheme.BaseColorScheme == "Dark" ? new MaterialDesignDarkTheme() : (IBaseTheme)new MaterialDesignLightTheme();
+            theme.SetBaseTheme(bTheme);
+            helper.SetTheme(theme);
+        }
+    }
+}
---
+++ ImageConvertor/Convertors/InvartConvertor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ImageFormatChanger
+{
+    public class InvartConvertor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                return !(bool)value;
+            }
+            else
+            {
+                throw new ArgumentException("Not boolean.");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                return !(bool)value;
+            }
+            else
+            {
+                throw new ArgumentException("Not boolean.");
+            }
+        }
+    }
+}
